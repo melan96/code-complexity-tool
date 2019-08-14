7241a02dc2b706dc66f2b29519dd2e431c6b5b02
melan96 <it17006712@my.sliit.lk>
2019-08-14 13:57:30 +0530

    Cnc calculation algorithm

--- CodeComplexityToolSPM/src/cnC_calculation/CncCalculation.java
+++ CodeComplexityToolSPM/src/cnC_calculation/CncCalculation.java
@@ -7,6 +7,8 @@ package cnC_calculation;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  *
@@ -45,6 +47,33 @@ public class CncCalculation {
     
     public void coreBracketMapper() {
         
+        
+        
+                Pattern bracketsPattern = Pattern.compile(bracketsREGX);
+		Pattern bracketsSingle = Pattern.compile(singleLineREGX);
+		Pattern bracketsClosed = Pattern.compile(closedBracketREGX);
+
+		// Check each line for Cnc
+		for (int i = 0; i < lineOfCode.size(); i++) {
+			int count = 0;
+			String line = lineOfCode.get(i);
+			// check for conditions and loops
+			Matcher matcher = bracketsPattern.matcher(line);
+			while (matcher.find()) {
+				incrementBracketState();
+			}
+			count = bracketState;
+			
+			Matcher close_m = bracketsClosed.matcher(line);
+			while (close_m.find()) {
+				decrementBracketState();
+			}
+			// check for lines with brackets or empty line
+			Matcher singleline = bracketsSingle.matcher(line);
+			if (singleline.find())
+				count = 0;
+			cncPoints.add(count);
+		}
     }
 
 
