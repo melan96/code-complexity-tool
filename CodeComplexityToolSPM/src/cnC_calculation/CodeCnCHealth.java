5c0eb317ebcdf4f5bfe4e470437cbb6cf4690b69
melan96 <it17006712@my.sliit.lk>
2019-08-14 14:12:32 +0530

    CncCodeHealth Class added to CnC branch

---
+++ CodeComplexityToolSPM/src/cnC_calculation/CodeCnCHealth.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cnC_calculation;
+
+/**
+ *
+ * @author melan
+ */
+public class CodeCnCHealth {
+    
+    //This class responsible for measure imported codebase CnC health
+    //validate imported code indentation
+    
+}
