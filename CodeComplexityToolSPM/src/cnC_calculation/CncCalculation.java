2f9534317dd72e707ddde15f29db706a6b0c1703
melan96 <it17006712@my.sliit.lk>
2019-08-14 13:23:24 +0530

    cnc implementation code

--- CodeComplexityToolSPM/src/cnC_calculation/CncCalculation.java
+++ CodeComplexityToolSPM/src/cnC_calculation/CncCalculation.java
@@ -5,10 +5,47 @@
  */
 package cnC_calculation;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 /**
  *
  * @author melan
  */
 public class CncCalculation {
     
+    //var base
+    ArrayList<String> lineOfCode;
+    ArrayList<Integer> cncPoints;
+    static LinkedHashMap<Integer,Integer> cncLinePoint = new LinkedHashMap<Integer,Integer>();
+    int bracketState = 0; //initially bracket state at globle value map
+    
+    //REGX block
+    String openBracketsREGX="\\{";
+    String closedBracketREGX= "\\}";
+    String bracketsREGX = "\\b((if|while|for|do)(\\s+|\\().*\\{)";
+    String singleLineREGX="^(\\s*\\}\\s*)|^(\\s*)$";
+    
+      public CncCalculation(ArrayList<String> lineOfCode){
+        this.lineOfCode = lineOfCode;
+        cncPoints = new ArrayList<Integer> (lineOfCode.size());
+    }
+    
+    public void incrementBracketState(){
+        System.out.println("increment state CNC");
+        this.bracketState++;
+    }
+    
+    public void decrementBracketState(){
+    
+        System.out.println("Decrement state CNC");
+        this.bracketState--;
+        
+    }
+    
+    public void coreBracketMapper() {
+        
+    }
+
+
 }
