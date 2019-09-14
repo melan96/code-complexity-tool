ab5a519dff66d9e636976792cd2e0a9b34c98db7
romeliyan <rkbliyanage@gmail.com>
2019-09-14 10:57:26 +0530

    CS Value Calculation for Java programs

--- CodeComplexityToolSPM/src/cs_calculation/cs_calculation.java
+++ CodeComplexityToolSPM/src/cs_calculation/cs_calculation.java
@@ -9,35 +9,50 @@ import codecomplexitytoolspm.Analysis;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author pasindu
- */
+
 public class cs_calculation {
     
-    private final String REGEX_TWO = "new|delete|throw|throws|&[^&]|\\*";
-    private final String REGEX_ARITHMETIC = "\\+[^\\+]|-[^-]| \\*|/[^/]|%|\\+\\+|--";
-    private final String REGEX_RELATION = "==|!=|>|<|>=|<=";
-    private final String REGEX_LOGICAL = "&&|\\|\\||!";
-    private final String REGEX_BITWISE = "\\|[^\\|]|\\^|~|<<|>>|<<<|>>>";
-    private final String RGEX_MISCELLANEOUS = ",|->|\\.|::";
-    private final String REGEX_ASSIGNMENT = "\\+=|-=|\\*=|[^<>]=|>>>=|\\|=|&=|%=|<<=|>>=|\\^=";
-    private final String REGEX_KEY = "void|double|int|float|String|printf|println|cout|cin|if|for|while|do-while|switch|case";
-    private final String REGEX_MANIPULATORS = "endl|\\n";
-    private final String REGEX_TEXT = "\".*?\"";
-    private final String REGEX_VARIABLE = "[a-z_]\\w*";
-    private final String REGEX_NUM = "\\d+";
-    private final String REGEX_REMOVE = "public|static|else|try|return|new|delete|throw|throws";
+    public static final String REGEX_TWO = "new|delete|throws|throw";
+    public static final String REGEX_ARITHMETIC = "[\\s]\\+[\\s]|[\\s]\\-[\\s]|[\\s]\\*[\\s]|[\\s]\\/[\\s]|[\\s]\\%[\\s]|\\+[\\+]|\\-[\\-]";
+    public static final String REGEX_RELATION = "=[\\=]|![\\=]|[\\s]>[\\s]|[\\s]<[\\s]|[\\s]>[\\=]|[\\s]<[\\=]";
+    public static final String REGEX_LOGICAL = "&[\\&]|\\|[\\|]|![\\s]";
+    public static final String REGEX_BITWISE = "[\\s]\\|[\\s]|<[\\<]|>[\\>]|<[\\<<]|>[\\>>]|[\\s]~[\\s]|[\\s]\\^[\\s]";
+    public static final String RGEX_MISCELLANEOUS = ",[\\s]|[\\s]->|::";
+    public static final String REGEX_ASSIGNMENT = "[\\s]\\+[\\=]|[\\s]\\-[\\=]|[\\s]\\*[\\=]|[\\s]\\/[\\=]|[\\s]\\&[\\=]";
+    public static final String REGEX_MANIPULATORS = "endl|\\n";
+    public static final String REGEX_TEXT = "\".*?\"";
+    public static final String REGEX_VARIABLE = "[a-z_]\\w*";
+    public static final String REGEX_NUM = "\\d+";
+    public static final String REGEX_REMOVE = "public|static|else|try|return|new|delete|throws|throw|class";
+    public static final String REGEX_IMPORTS = "\\bimport\\b";
+    public static final String REGEX_COMMENTS = "[\\/\\/.*]";
     
-    private final Pattern pattern = Pattern.compile("(" + REGEX_ARITHMETIC + "|" + REGEX_RELATION + "|" + REGEX_LOGICAL + "|" + REGEX_BITWISE + "|" + RGEX_MISCELLANEOUS + "|" + REGEX_MANIPULATORS + "|" + REGEX_TEXT + "|" + REGEX_VARIABLE + "|" + REGEX_NUM + ")");
-    private final Pattern removePattern = Pattern.compile("(" + REGEX_REMOVE + ")");
-    private final Pattern doublePattern = Pattern.compile("(" + REGEX_TWO + ")");
+    
+    private final Pattern pattern = Pattern.compile (REGEX_TWO);
+    private final Pattern ArithmeticPattern = Pattern.compile (REGEX_ARITHMETIC);
+    private final Pattern RelationPattern = Pattern.compile (REGEX_RELATION);
+    private final Pattern LogicalPattern = Pattern.compile (REGEX_LOGICAL);
+    private final Pattern BitwisePattern = Pattern.compile (REGEX_BITWISE);
+    private final Pattern MisPattern = Pattern.compile (RGEX_MISCELLANEOUS);
+    private final Pattern AssignmentPattern = Pattern.compile (REGEX_ASSIGNMENT);
+    private final Pattern ManuPattern = Pattern.compile (REGEX_MANIPULATORS);
+    private final Pattern TextPattern = Pattern.compile (REGEX_TEXT);
+    private final Pattern VariablePattern = Pattern.compile (REGEX_VARIABLE);
+    private final Pattern NumPattern = Pattern.compile (REGEX_NUM);
+    private final Pattern RemovePattern = Pattern.compile (REGEX_REMOVE);
+    private final Pattern ImportPattern = Pattern.compile (REGEX_IMPORTS);
+    private final Pattern CommentPattern = Pattern.compile (REGEX_COMMENTS);
+    
+    
+   
     
     public cs_calculation(){
         
     }
        
     public void csCalculate(){
+        
+        System.out.println("Inside cs calculate method");
         
         for(int i=0; i < Analysis.resultSet.size(); i++){
             
@@ -46,22 +61,94 @@ public class cs_calculation {
             String currentLine = Analysis.resultSet.get(i).getLineContent();
             
             final Matcher matcher = pattern.matcher(currentLine);
-            final Matcher removeMatcher = removePattern.matcher(currentLine);
-            final Matcher doubleMatcher = doublePattern.matcher(currentLine);
+            final Matcher arithmeticMatcher = ArithmeticPattern.matcher(currentLine);
+            final Matcher relationMatcher = RelationPattern.matcher(currentLine);
+            final Matcher logicalMatcher = LogicalPattern.matcher(currentLine);
+            final Matcher bitwiseMatcher = BitwisePattern.matcher(currentLine);
+            final Matcher misMatcher = MisPattern.matcher(currentLine);
+            final Matcher assignmentMatcher = AssignmentPattern.matcher(currentLine);
+            final Matcher manuMatcher = ManuPattern.matcher(currentLine);
+            final Matcher textMatcher = TextPattern.matcher(currentLine);
+            final Matcher variableMatcher = VariablePattern.matcher(currentLine);
+            final Matcher numMatcher = NumPattern.matcher(currentLine);
+            final Matcher removeMatcher = RemovePattern.matcher(currentLine);
+            final Matcher importMatcher = ImportPattern.matcher(currentLine);
+             final Matcher commentMatcher = CommentPattern.matcher(currentLine);
+           
             
             while(matcher.find()){
-                
+              //  System.out.println("svalue increasing");
+                csValue++;
+                csValue++;
+            }
+            
+            while(arithmeticMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+             
+            while(relationMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+             
+            while(logicalMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+            
+            while(bitwiseMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+             
+            while(misMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+            
+            while(assignmentMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+            
+            while(manuMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+            
+            while(textMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+            
+            while(variableMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue++;
+            }
+            
+            while(numMatcher.find()){
+              //  System.out.println("svalue increasing");
                 csValue++;
             }
             
             while(removeMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue--;
+            }
+             
+            while(importMatcher.find()){
+              //  System.out.println("svalue increasing");
                 csValue--;
             }
             
-            while(doubleMatcher.find()){
-                
-                csValue =+ 2;
+            while(commentMatcher.find()){
+              //  System.out.println("svalue increasing");
+                csValue--;
             }
+            
+            
+            
             
             
             System.out.println("CS Value: " + csValue);
@@ -71,40 +158,6 @@ public class cs_calculation {
             
         }
         
-        
-    }
-    
-    public int CsCalculateChunk(String codeStatement){
-        
-       
-            
-            int csValue = 0;
-            
-            String currentLine = codeStatement;
-            
-            final Matcher matcher = pattern.matcher(codeStatement);
-            final Matcher removeMatcher = removePattern.matcher(codeStatement);
-            final Matcher doubleMatcher = doublePattern.matcher(codeStatement);
-            
-            while(matcher.find()){
-                csValue++;
-            }
-            
-            while(removeMatcher.find()){
-                csValue--;
-            }
-            
-            while(doubleMatcher.find()){
-                
-                csValue =+ 2;
-            }
-            
-            
-            System.out.println("[TEST] CS Value: " + csValue);
-            
-            return csValue;
-            
-            
         
     }
     
