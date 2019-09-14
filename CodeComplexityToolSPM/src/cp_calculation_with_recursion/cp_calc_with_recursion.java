76dd297a09710289b2dfd8f1c83e0c21edca2c0f
Pasindu Piumal <pasindu0996@gmail.com>
2019-09-14 20:28:34 +0530

    CP With Recursion Functionality

--- CodeComplexityToolSPM/src/cp_calculation_with_recursion/cp_calc_with_recursion.java
+++ CodeComplexityToolSPM/src/cp_calculation_with_recursion/cp_calc_with_recursion.java
@@ -5,10 +5,25 @@
  */
 package cp_calculation_with_recursion;
 
+import codecomplexitytoolspm.Analysis;
+import codecomplexitytoolspm.ProgramStatement;
+
 /**
  *
  * @author Romesh
  */
 public class cp_calc_with_recursion {
-    
+    public static int cpTotal=0;
+    public void calculatesCpValues(){
+   
+        for(ProgramStatement progs : Analysis.resultSet){
+            if(progs.getCrValue()==0){
+                progs.setCpValue(progs.getCpsValue());
+            }
+            else{
+                progs.setCpValue(progs.getCpsValue() + progs.getCrValue());
+            }
+            cpTotal=cpTotal+progs.getCpValue();
+        }
+    }
 }
